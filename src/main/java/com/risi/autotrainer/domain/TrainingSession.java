4613c7ee6150e020138cd57f407864c4e9ee5ccb
alexandrerisi <alexandrerisi@gmail.com>
2019-03-28 16:25:34 +0000

    Display previous sessions done.

--- src/main/java/com/risi/autotrainer/domain/TrainingSession.java
+++ src/main/java/com/risi/autotrainer/domain/TrainingSession.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 public class TrainingSession {
@@ -15,4 +16,17 @@ public class TrainingSession {
     private List<ExerciseSet> sets = new ArrayList<>();
     private LocalDateTime date;
     private String userId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrainingSession that = (TrainingSession) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date);
+    }
 }
--- src/main/java/com/risi/autotrainer/ui/PreviousWorkoutsUI.java
+++ src/main/java/com/risi/autotrainer/ui/PreviousWorkoutsUI.java
@@ -1,5 +1,7 @@
 package com.risi.autotrainer.ui;
 
+import com.risi.autotrainer.domain.ExerciseSet;
+import com.risi.autotrainer.domain.TrainingSession;
 import com.risi.autotrainer.service.TrainingSessionService;
 import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.ClickEvent;
@@ -7,17 +9,29 @@ import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-class PreviousWorkoutsUI extends HorizontalLayout {
+class PreviousWorkoutsUI extends VerticalLayout {
 
     private DatePicker to;
     private DatePicker from;
     private Button search;
+    private VerticalLayout contentLayout = new VerticalLayout();
+    private List<TrainingSession> trainingSessions;
 
     PreviousWorkoutsUI(TrainingSessionService trainingSessionService) {
+
+        HorizontalLayout selectorLayout = new HorizontalLayout();
+        add(selectorLayout);
 
         from = new DatePicker();
         from.setMin(LocalDate.of(2019, 2, 1));
@@ -34,23 +48,65 @@ class PreviousWorkoutsUI extends HorizontalLayout {
         to.setMax(LocalDate.now());
         to.addValueChangeListener(
                 (HasValue.ValueChangeListener<AbstractField.ComponentValueChangeEvent<DatePicker, LocalDate>>) ev -> {
-            if (from.getValue() != null && !from.getValue().isBefore(to.getValue()))
-                from.setValue(null);
-            from.setMax(to.getValue().minusDays(1));
-            searchButtonActivation();
-        });
+                    if (from.getValue() != null && !from.getValue().isBefore(to.getValue()))
+                        from.setValue(null);
+                    from.setMax(to.getValue().minusDays(1));
+                    searchButtonActivation();
+                });
         to.setLabel("To Day");
 
         search = new Button("Search");
-        search.addClickListener((ComponentEventListener<ClickEvent<Button>>) event ->
-                trainingSessionService.getTrainingSessionByDate(from.getValue(), to.getValue()));
+        search.addClickListener((ComponentEventListener<ClickEvent<Button>>) event -> {
+            trainingSessions = trainingSessionService.getTrainingSessionByDate(from.getValue(), to.getValue());
+            contentLayout.removeAll();
+            if (trainingSessions != null) {
+                loadTrainingSessions(trainingSessions);
+            }
+        });
         search.setEnabled(false);
 
-        add(from, to, search);
+        selectorLayout.add(from, to, search);
+        add(selectorLayout);
+        add(contentLayout);
     }
 
     private void searchButtonActivation() {
         if (from.getValue() != null && to.getValue() != null)
             search.setEnabled(true);
+    }
+
+    private void loadTrainingSessions(List<TrainingSession> sessions) {
+
+        List<List<TrainingSession>> sessionsByDay = createSessionsListByDay(sessions);
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE;
+
+        for (List<TrainingSession> daySession : sessionsByDay) {
+            var grid = new Grid<>(ExerciseSet.class);
+            contentLayout.add(new Label(daySession.get(0).getDate().format(dtf)), grid);
+
+            var exerciseSets = new ArrayList<ExerciseSet>();
+            for (TrainingSession ts : daySession)
+                exerciseSets.addAll(ts.getSets());
+
+            grid.setItems(exerciseSets);
+        }
+    }
+
+    private List<List<TrainingSession>> createSessionsListByDay(List<TrainingSession> sessions) {
+
+        var returnList = new ArrayList<List<TrainingSession>>();
+
+        LocalDateTime localDateTime = null;
+        List<TrainingSession> sessionList = null;
+        for (TrainingSession ts : sessions) {
+            if (localDateTime == null || !localDateTime.isEqual(ts.getDate())) {
+                sessionList = new ArrayList<>();
+                returnList.add(sessionList);
+                localDateTime = ts.getDate();
+            }
+            sessionList.add(ts);
+        }
+
+        return returnList;
     }
 }
